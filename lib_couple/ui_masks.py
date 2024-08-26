0df3289ce928b08bfbf11fa57183b6b162d7fb04
Haoming <hmstudy02@gmail.com>
2024-08-26 18:17:01 +0800

    support Gradio 4

--- lib_couple/ui_masks.py
+++ lib_couple/ui_masks.py
@@ -5,6 +5,11 @@ import numpy as np
 from .gr_version import js, is_gradio_4
 from .ui_funcs import COLORS
 
+try:
+    from modules_forge.forge_canvas.canvas import ForgeCanvas
+except ModuleNotFoundError:
+    pass
+
 
 class CoupleMaskData:
 
@@ -176,7 +181,123 @@ class CoupleMaskData:
 
     def _mask_ui_4(self, btn, res, mode) -> list[gr.components.Component]:
         """The UI for the "Neo" Forge based on Gradio 4.x"""
-        raise NotImplementedError
+
+        # ===== Components ===== #
+        msk_btn_empty = gr.Button("Create Empty Canvas", elem_classes="round-btn")
+
+        msk_canvas = ForgeCanvas(scribble_color="#FFFFFF")
+
+        with gr.Row(elem_classes="fc_msk_io"):
+            msk_btn_save = gr.Button(
+                "Save Mask ", interactive=True, elem_classes="round-btn"
+            )
+            msk_btn_load = gr.Button(
+                "Load Mask", interactive=False, elem_classes="round-btn"
+            )
+            msk_btn_override = gr.Button(
+                "Override Mask", interactive=False, elem_classes="round-btn"
+            )
+
+        with gr.Row(visible=False):
+            operation = gr.Textbox(interactive=True, elem_classes="fc_msk_op")
+            operation_btn = gr.Button("op", elem_classes="fc_msk_op_btn")
+
+        gr.HTML('<div class="fc_masks"></div>')
+
+        msk_preview = gr.Image(
+            show_label=False,
+            image_mode="RGB",
+            type="pil",
+            interactive=False,
+            show_download_button=False,
+            elem_classes="fc_msk_preview",
+        )
+
+        msk_gallery = gr.Gallery(
+            show_label=False,
+            show_share_button=False,
+            show_download_button=False,
+            interactive=False,
+            visible=False,
+            elem_classes="fc_msk_gal",
+        )
+
+        msk_btn_reset = gr.Button("Reset All Masks", elem_classes="round-btn")
+
+        weights_field = gr.Textbox(visible=False, elem_classes="fc_msk_weights")
+        # ===== Components ===== #
+
+        # ===== Events ===== #
+        msk_btn_empty.click(
+            self._create_empty,
+            res,
+            [msk_canvas.background, msk_canvas.foreground],
+        )
+
+        msk_btn_save.click(
+            self._write_mask,
+            msk_canvas.foreground,
+            [msk_gallery, msk_preview, msk_btn_load, msk_btn_override],
+        ).success(
+            fn=None, **js(f'() => {{ ForgeCouple.populateMasks("{self.mode}"); }}')
+        )
+
+        msk_btn_override.click(
+            self._override_mask,
+            msk_canvas.foreground,
+            [msk_gallery, msk_preview, msk_btn_load, msk_btn_override],
+        ).success(
+            fn=None, **js(f'() => {{ ForgeCouple.populateMasks("{self.mode}"); }}')
+        )
+
+        msk_btn_load.click(self._load_mask, None, msk_canvas.foreground)
+
+        msk_btn_reset.click(
+            self._reset_masks,
+            None,
+            [msk_gallery, msk_preview, msk_btn_load, msk_btn_override],
+        ).success(
+            fn=None, **js(f'() => {{ ForgeCouple.populateMasks("{self.mode}"); }}')
+        )
+
+        weights_field.change(self._write_weights, weights_field)
+
+        operation_btn.click(
+            self._on_operation,
+            operation,
+            [msk_gallery, msk_preview, msk_btn_load, msk_btn_override],
+        ).success(
+            fn=None, **js(f'() => {{ ForgeCouple.populateMasks("{self.mode}"); }}')
+        )
+
+        btn.click(
+            self._refresh_resolution,
+            [res, mode],
+            [msk_gallery, msk_preview, msk_canvas.background, msk_canvas.foreground],
+        ).success(
+            fn=None, **js(f'() => {{ ForgeCouple.populateMasks("{self.mode}"); }}')
+        )
+        # ===== Events ===== #
+
+        # ===== Pain ===== #
+        [
+            setattr(comp, "do_not_save_to_config", True)
+            for comp in (
+                msk_btn_empty,
+                msk_canvas,
+                msk_canvas.foreground,
+                msk_canvas.background,
+                msk_btn_save,
+                msk_btn_load,
+                msk_btn_override,
+                operation,
+                operation_btn,
+                msk_preview,
+                msk_gallery,
+                msk_btn_reset,
+                weights_field,
+            )
+        ]
 
     @staticmethod
     def _parse_resolution(resolution: str) -> tuple[int, int]:
@@ -192,7 +313,11 @@ class CoupleMaskData:
     def _create_empty(resolution: str) -> Image.Image:
         """Generate a blank black canvas"""
         w, h = CoupleMaskData._parse_resolution(resolution)
-        return Image.new("RGB", (w, h))
+
+        if is_gradio_4:
+            return [Image.new("RGB", (w, h)), None]
+        else:
+            return Image.new("RGB", (w, h))
 
     def _on_operation(self, op: str) -> list[list, Image.Image, bool, bool]:
         """Operations triggered from JavaScript"""
@@ -244,16 +369,27 @@ class CoupleMaskData:
         self, resolution: str, mode: str
     ) -> list[list, Image.Image, Image.Image]:
         """Refresh when width or height is changed"""
-        canvas = self._create_empty(resolution)
+
+        if is_gradio_4:
+            (canvas, _) = self._create_empty(resolution)
+        else:
+            canvas = self._create_empty(resolution)
 
         if mode != "Mask" or not self.masks:
-            return [gr.update(), gr.update(), canvas]
+            if is_gradio_4:
+                return [gr.update(), gr.update(), canvas, None]
+            else:
+                return [gr.update(), gr.update(), canvas]
 
         w, h = self._parse_resolution(resolution)
 
         self.masks = [mask.resize((w, h)) for mask in self.masks]
         preview = self._generate_preview()
-        return [self.masks, preview, canvas]
+
+        if is_gradio_4:
+            return [self.masks, preview, canvas, None]
+        else:
+            return [self.masks, preview, canvas]
 
     def _reset_masks(self) -> list[list, Image.Image, bool, bool]:
         """Clear everything"""
